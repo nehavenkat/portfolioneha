a470b2bbe23dd7c416a089a8ce2ccdd4e7452caa
neha venkat <venkatsetty.neha@gmail.com>
2020-04-17 17:02:17 +0200

    updated

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -1,10 +1,24 @@
 import React, { Component } from "react";
+import "../AboutMe/AboutMe.css";
 
+import { Grid, Cell } from "react-mdl";
 export default class AboutMe extends Component {
   render() {
     return (
       <div>
-        <h1>About ME</h1>
+        <div className="Container">
+          <Grid>
+            <Cell col={2}></Cell>
+            <Cell className="resume-right-col" col={8}>
+              <h1>I am Neha Venkat Setty.</h1>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s"
+              </p>
+            </Cell>
+          </Grid>
+        </div>
       </div>
     );
   }
