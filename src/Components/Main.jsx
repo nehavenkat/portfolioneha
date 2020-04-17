52dabfca8386a7de941f5f29bc2c9cad8638ac75
neha venkat <venkatsetty.neha@gmail.com>
2020-04-17 13:25:48 +0200

    abbed routes

---
+++ src/Components/Main.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import LandingPage from "../Components/LandingPage/LandingPage";
+import AboutMe from "../Components/AboutMe/AboutMe";
+import Contact from "../Components/Contact/Contact";
+import NavBar from "../Components/NavBar/NavBar";
+
+export default class Main extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <NavBar />
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/aboutme" component={AboutMe} />
+            <Route path="/contact" component={Contact} />
+            {/* <Route path="/projects" component={Projects} />
+    <Route path="/resume" component={Resume} /> */}
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
+}
