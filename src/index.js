9e2bb137f80af2a9607a3906678ac8062da7f2b4
neha venkat <venkatsetty.neha@gmail.com>
2020-04-29 11:11:04 +0200

    responsive mobile

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-//import App from "./App";
 import "react-mdl/extra/material.css";
 import "react-mdl/extra/material.js";
 import { BrowserRouter } from "react-router-dom";
